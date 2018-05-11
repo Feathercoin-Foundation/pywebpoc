72f879210cea9feeac12094a4f784bd36025f262
Lucas Betschart <lucasbetschart@gmail.com>
2018-05-11 15:30:02 +0200

    Update NWJS

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -x
 
-NWJS_URL="https://dl.nwjs.io/v0.30.1/nwjs-v0.30.1-win-ia32.zip"
+NWJS_URL="https://dl.nwjs.io/v0.30.3/nwjs-v0.30.3-win-ia32.zip"
 PYINSTALLER="wine c:/python3.5.4/scripts/pyinstaller.exe"
 PYTHON="wine c:/python3.5.4/python.exe"
 if [ "$(uname -s)" = "Darwin" ]; then
-    NWJS_URL="https://dl.nwjs.io/v0.30.1/nwjs-v0.30.1-osx-x64.zip"
+    NWJS_URL="https://dl.nwjs.io/v0.30.3/nwjs-v0.30.3-osx-x64.zip"
     PYINSTALLER=pyinstaller
     PYTHON=python
 fi
